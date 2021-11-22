0873976b9bcf9e7b43334d6668d832ec01aa0f99
AJOsmaston <dajoxx@gmail.com>
2021-11-22 17:30:40 +0000

    statement formatter on account_log

--- spec/account_log.test.js
+++ spec/account_log.test.js
@@ -1,16 +1,16 @@
 const AccountLog = require('../lib/account_log')
 
 describe('Account Log', () => {
+  beforeEach(() => {
+    this.account_log = new AccountLog
+    this.default_formatted_date = '01/01/1970';
+  })
+
+  afterEach(() => {
+    jest.spyOn(global.Date, 'now').mockRestore();
+  })
+
   describe('Date Logs', () => {
-    beforeEach(() => {
-      this.account_log = new AccountLog
-      this.default_formatted_date = '01/01/1970';
-    })
-
-    afterEach(() => {
-      jest.spyOn(global.Date, 'now').mockRestore();
-    })
-
     it('exists on a deposit', () => {
       jest.spyOn(global.Date, 'now').mockReturnValue(0);
       this.account_log.date_deposit();
@@ -39,4 +39,22 @@ describe('Account Log', () => {
       expect(this.account_log.withdrawal_log).toContain(10)
     });
   });
+
+  describe('#produce_statement', () => {
+    it('produces formatted statement', () => {
+      jest.spyOn(global.Date, 'now').mockReturnValue(1673308800000);
+      this.account_log.deposit(1000)
+      jest.spyOn(global.Date, 'now').mockReturnValue(1673568000000);
+      this.account_log.deposit(2000)
+      jest.spyOn(global.Date, 'now').mockReturnValue(1673654400000);
+      this.account_log.withdraw(500)
+
+      expect(this.account_log.statement).toEqual([
+        "date || credit || debit ",
+        "14/01/2023 || || 500.00 ",
+        "13/01/2023 || 2000.00 || ",
+        "10/01/2023 || 1000.00 || "
+      ])
+    })
+  })
 });
