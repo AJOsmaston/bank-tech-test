8ac87f441ab3f77ccc82425685bfaeff9333e6e2
AJOsmaston <dajoxx@gmail.com>
2021-11-22 15:13:25 +0000

    changed 'bank' to 'account'

--- spec/bank.test.js
+++ spec/bank.test.js
@@ -1,48 +1,50 @@
-const Bank = require('../lib/bank');
+const Account = require('../lib/account');
 
-describe('Bank Tests', () => {
+describe('Account Tests', () => {
   beforeEach(() => {
-    this.bank = new Bank();
-    this.test_balance = this.bank.balance;
+    this.account = new Account();
+    this.test_balance = this.account.balance;
   });
 
   describe('Deposit', () => {
     it('allows a deposit', () => {
       this.test_balance += 5;
-      this.bank.deposit(5);
+      this.account.deposit(5);
 
-      expect(this.bank.balance).toEqual(this.test_balance);
+      expect(this.account.balance).toEqual(this.test_balance);
     });
   });
 
   describe('Withdraw', () => {
     it('allows a withdrawal', () => {
       this.test_balance -= 5;
-      this.bank.withdraw(5);
+      this.account.withdraw(5);
 
-      expect(this.bank.balance).toEqual(this.test_balance);
+      expect(this.account.balance).toEqual(this.test_balance);
     });
   });
 
   describe('Datestamp', () => {
+    beforeEach(() => {
+      this.default_formatted_date = '01/01/1970'
+    })
+
     afterEach(() => {
       jest.spyOn(global.Date, 'now').mockRestore();
     })
 
     it('exists on a deposit', () => {
       jest.spyOn(global.Date, 'now').mockReturnValue(0);
-      let default_formatted_date = '01/01/1970'
 
-      this.bank.deposit(5);
-      expect(this.bank.deposit_log).toContain(default_formatted_date)
+      this.account.deposit(5);
+      expect(this.account.deposit_log).toContain(this.default_formatted_date)
     });
 
     it('exists on a withdrawal', () => {
       jest.spyOn(global.Date, 'now').mockReturnValue(0);
-      let default_formatted_date = '01/01/1970'
 
-      this.bank.withdraw(5);
-      expect(this.bank.withdrawal_log).toContain(default_formatted_date)
+      this.account.withdraw(5);
+      expect(this.account.withdrawal_log).toContain(this.default_formatted_date)
     })
   });
 });
